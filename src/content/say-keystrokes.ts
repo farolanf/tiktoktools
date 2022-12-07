25f7e3c1223bd773e5dabc64a845333a3e0280e2
Farolan Faisal <farolanfaisal@gmail.com>
2022-12-07 01:32:58 +0000

    organize content scripts

---
+++ src/content/say-keystrokes.ts
@@ -0,0 +1,14 @@
+import debounce from 'lodash/debounce';
+import { MessageType, sendMessage } from '../lib/message';
+
+let keystrokes = '';
+
+const say = debounce(() => {
+  sendMessage({ type: MessageType.SPEECH, text: keystrokes });
+  keystrokes = '';
+}, 500);
+
+window.addEventListener('keyup', (e) => {
+  keystrokes += e.key;
+  say();
+});
