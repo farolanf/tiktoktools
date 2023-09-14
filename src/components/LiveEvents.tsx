b058d03172b0c565552b9daea0791fb9b0e9039e
Farolan Faisal <farolanfaisal@gmail.com>
2023-09-14 13:42:13 +0000

    feat: sanitize comment feat: say gift

--- src/components/LiveEvents.tsx
+++ src/components/LiveEvents.tsx
@@ -6,9 +6,26 @@ import { isSpeaking, say } from '../lib/speech';
 
 const API_URL = 'http://192.168.1.7:9003'
 
+const KURS = 15000
+const COIN_RP = KURS / 200
+
+const getRp = e => e.coins * e.count * COIN_RP
+
+const sanitizeComment = str => str.replaceAll(/[^a-zA-Z0-9.,!? ]+/g, '')
+
+const sayGift = e => {
+    const coins = e.coins * e.count
+    if (coins > 100) say(`wow! wooooww!! woooooooooooooowwwww!!!! terima kasih banyakk kakak ${e.user}!`)
+    else if (coins > 50) say(`wow! terima kasih banyakk kakak ${e.user}`)
+    else if (coins > 10) say(`terima kasih kakak ${e.user}`)
+    else say(`makasih kak ${e.user}`)
+}
+
 export default function LiveEvents() {
     const [userId, setUserId] = useState('')
     const [event, setEvent] = useState<any>()
+    const [comment, setComment] = useState('')
+    const [gift, setGift] = useState('')
 
     useEffect(() => {
         fetchInfo()
@@ -49,9 +66,12 @@ export default function LiveEvents() {
             }
             switch (event.type) {
                 case 'comment':
-                    say(event.comment)
+                    say(sanitizeComment(event.comment))
+                    setComment(`${event.user}: ${event.comment}`)
                     break
                 case 'gift':
+                    sayGift(event)
+                    setGift(`${event.user}: ${event.gift} x ${event.count} = ${event.coins * event.count} @${event.coins} = Rp${getRp(event)}`)
                     break
             }
         }
@@ -93,13 +113,40 @@ export default function LiveEvents() {
             </Form>
             <Form.Group controlId="formGroupLiveEventTest">
                 <Form.Label>Live Events</Form.Label>
-                {event?.type == 'comment' && <Form.Control
-                    type="text"
-                    className="mb-2"
-                    value={`${event.user}: ${event.comment}`}
-                    readOnly
-                    style={{ width: 800 }}
-                />}
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Form.Label
+                        style={{
+                            marginRight: 8,
+                            fontWeight: event?.type == 'comment' ? 700 : 400
+                        }}
+                    >
+                        Comment
+                    </Form.Label>
+                    <Form.Control
+                        type="text"
+                        className="mb-2"
+                        value={comment}
+                        readOnly
+                        style={{ width: 800 }}
+                    />
+                </div>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Form.Label
+                        style={{
+                            marginRight: 8,
+                            fontWeight: event?.type == 'gift' ? 700 : 400
+                        }}
+                    >
+                        Gift
+                    </Form.Label>
+                    <Form.Control
+                        type="text"
+                        className="mb-2"
+                        value={gift}
+                        readOnly
+                        style={{ width: 800 }}
+                    />
+                </div>
             </Form.Group>
         </>
     )
