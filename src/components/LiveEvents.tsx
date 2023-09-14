75ee1fc3b8d3e547d96c16469b0e08a441a99a65
Farolan Faisal <farolanfaisal@gmail.com>
2023-09-14 12:57:47 +0000

    fix: fix form refresh

--- src/components/LiveEvents.tsx
+++ src/components/LiveEvents.tsx
@@ -1,37 +1,106 @@
 import { useEffect, useRef, useState } from "react"
+import Stack from 'react-bootstrap/Stack';
 import { Form } from "./Options"
+import Button from 'react-bootstrap/Button';
+import { isSpeaking, say } from '../lib/speech';
 
 const API_URL = 'http://192.168.1.7:9003'
 
 export default function LiveEvents() {
-    const [events, setEvents] = useState([])
-    const currentRef = useRef<any>()
-
-    currentRef.current = { events }
+    const [userId, setUserId] = useState('')
+    const [event, setEvent] = useState<any>()
 
     useEffect(() => {
+        fetchInfo()
         fetchEvents()
     }, [])
 
-    async function fetchEvents() {
-        fetch(API_URL + '/events')
+    useEffect(() => {
+        maybeSay(event)
+    }, [event])
+
+    async function fetchInfo() {
+        await fetch(API_URL + '/info')
             .then(res => res.json())
             .then(json => {
-                const { events } = currentRef.current
-                setEvents(events.concat(json))
-                setTimeout(fetchEvents, 500)
+                setUserId(json?.user_id ?? '')
             })
     }
 
+    async function connect(e) {
+        e.preventDefault()
+
+        if (!userId || userId.trim() == '') return
+
+        const fd = new FormData()
+        fd.append('user_id', userId)
+
+        return fetch(API_URL + '/live', {
+            method: 'post',
+            body: fd
+        })
+    }
+
+    async function maybeSay(event) {
+        if (event) {
+            if (await isSpeaking()) {
+                setTimeout(() => maybeSay(event), 200)
+                return
+            }
+            switch (event.type) {
+                case 'comment':
+                    say(event.comment)
+                    break
+                case 'gift':
+                    break
+            }
+        }
+    }
+
+    async function fetchEvents() {
+        if (await isSpeaking()) {
+            setTimeout(fetchEvents, 200)
+            return
+        }
+
+        await fetch(API_URL + '/events')
+            .then(res => res.json())
+            .then(json => json[0] && setEvent(json[0]))
+
+        setTimeout(fetchEvents, 200)
+    }
+
     return (
-        <Form.Group controlId="formGroupLiveEventTest">
-            <Form.Label>Live Events</Form.Label>
-            <textarea
-                readOnly
-                value={JSON.stringify(events, null, 4)}
-                rows={20}
-                style={{ width: 800 }}
-            />
-        </Form.Group>
+        <>
+            <Form onSubmit={connect}>
+                <Form.Group controlId="formGroupLiveEventTest">
+                    <Form.Label>User ID</Form.Label>
+                    <Form.Control
+                        type="text"
+                        className="mb-2"
+                        value={userId}
+                        onChange={(e) => setUserId(e.target.value)}
+                        onFocus={(e) => {
+                            e.target.setSelectionRange(0, e.target.value.length);
+                        }}
+                    />
+                    <Stack gap={2} className="align-items-start">
+                        <Button type="submit" variant="primary" disabled={!userId}>
+                            Connect
+                        </Button>
+                    </Stack>
+                </Form.Group>
+            </Form>
+            <Form.Group controlId="formGroupLiveEventTest">
+                <Form.Label>Live Events</Form.Label>
+                {event?.type == 'comment' && <Form.Control
+                    type="text"
+                    className="mb-2"
+                    value={`${event.user}: ${event.comment}`}
+                    readOnly
+                    style={{ width: 800 }}
+                />}
+            </Form.Group>
+        </>
     )
 }
--- src/lib/speech.ts
+++ src/lib/speech.ts
@@ -3,3 +3,7 @@ import { MessageType, sendMessage } from './message';
 export function say(text: string, voiceName?: string) {
   sendMessage({ type: MessageType.SPEECH, text, voiceName });
 }
+
+export async function isSpeaking() {
+  return await new Promise<boolean>((r) => chrome.tts.isSpeaking(r))
+}
